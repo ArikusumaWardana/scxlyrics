dd6ba745e9ad11811751fc8723ca69dbc3db574c
Arikusuma Wardana <87961962+ArikusumaWardana@users.noreply.github.com>
2024-05-16 10:13:13 +0800

    Menyesuaikan parameter user

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -13,13 +13,13 @@
         public function register() {
             $password = password_hash(htmlspecialchars($_POST['password']), PASSWORD_BCRYPT);
 
-            $query = "INSERT INTO {$this->tbUser}(name, username, email_user, password) VALUES(:name, :username, :email_user, :password)";
+            $query = "INSERT INTO {$this->tbUser}(name, username, user_email, user_password) VALUES(:name, :username, :user_email, :user_password)";
 
             $this->db->query($query);
             $this->db->bind('name', htmlspecialchars($_POST['name']));
             $this->db->bind('username', htmlspecialchars($_POST['username']));
-            $this->db->bind('email_user', htmlspecialchars($_POST['email_user']));
-            $this->db->bind('password', $password);
+            $this->db->bind('user_email', htmlspecialchars($_POST['email_user']));
+            $this->db->bind('user_password', $password);
 
             $this->db->execute();
             return $this->db->rowCount();
@@ -27,17 +27,17 @@
 
         public function getUserbyEmail($email) {
 
-            $query = "SELECT * FROM {$this->tbUser} WHERE email_user = :email_user";
+            $query = "SELECT * FROM {$this->tbUser} WHERE user_email = :user_email";
 
             $this->db->query($query);
-            $this->db->bind('email_user', $email);
+            $this->db->bind('user_email', $email);
             $this->db->execute();
             return $this->db->singleResult();
         }
 
         public function allUser() {
 
-            $query = "SELECT * FROM user ORDER BY id_user DESC";
+            $query = "SELECT * FROM user ORDER BY user_id DESC";
 
             $this->db->query($query);
             $this->db->execute();
